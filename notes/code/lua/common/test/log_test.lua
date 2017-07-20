6b3585560d7becd19e4462e515f64da4da2eedd6
root <root@DESKTOP-LKRP7OC.localdomain>
2017-07-20 22:14:49 +0800

    xx

--- notes/code/lua/common/test/log_test.lua
+++ notes/code/lua/common/test/log_test.lua
@@ -1,6 +1,7 @@
-package.path=package.path .. ";/root/workspace/xiaoyao/notes/code/lua/common/?.lua"
+package.path=package.path .. ";/root/workspace/notebook/notes/code/lua/common/?.lua"
 --print(package.path)
 
 require("log")
+
 
 log("hello world")
